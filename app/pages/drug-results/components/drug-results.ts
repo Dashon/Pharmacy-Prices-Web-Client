beb4b98393abd295b62568298cc1cf3420205570
Dashon Howard <dhoward@pathf.com>
2016-06-24 17:54:31 -0500

    update

--- app/pages/drug-results/components/drug-results.ts
+++ app/pages/drug-results/components/drug-results.ts
@@ -18,86 +18,67 @@ export const AUTOCOMPLETE_DIRECTIVES = [Autocomplete, AutocompleteContainer];
 
 })
 export class DrugResultsComp {
+    doses = [];
+    http = null;
+    response = null;
+    status:{isopen:boolean} = {isopen: false};
 
-  http = null;
-  response = null;
-  status:{isopen:boolean} = {isopen: false};
+    typeaheadLoading:boolean = false;
+    typeaheadNoResults:boolean = false;
+    public getAsyncDataRef = this.getAsyncData.bind(this);
+    public searchString:string = '';
 
-  autocompleteLoading:boolean = false;
-  autocompleteNoResults:boolean = false;
-
-
-  private _cachedResult:any;
-  private _previousContext:any;
- // private carsExample1:Array<string> = ['BMW', 'Audi','Mercedes','Porsche','Volkswagen','Opel','Maserati','Volkswagen','BMW Serie 2'];
-  toggleDropdown($event:MouseEvent):void {
-    $event.preventDefault();
-    $event.stopPropagation();
-    this.status.isopen = !this.status.isopen;
-  }
-
-  constructor(http:Http) {
-    this.http = http;
-  }
-
-  getDrugs(text) {
-    var authHeader = new Headers();
-    authHeader.append('X-Api-Key', '9a04cee7-09d3-4372-8f56-1713ac445fff');
-
-    var options = new RequestOptions({headers: authHeader});
-
-    return this.callApi('http://doc-and-i-api.herokuapp.com/api/v1/drugs?name_prefix='+text, options);
-  }
-
-  getUsers() {
-    var authHeader = new Headers();
-    authHeader.append('X-Api-Key', '9a04cee7-09d3-4372-8f56-1713ac445fff');
-
-    var options = new RequestOptions({headers: authHeader});
-
-    this.callApi('http://doc-and-i-api.herokuapp.com/api/v1/pharmacies?distance_from_zip=60640', options);
-  }
-
-  callApi(url, options) {
-    return this.http.get(url, options);
-
-  }
-
-  getCurrentContext() {
-    return this;
-  }
-
-  getAsyncData(context:any):Function {
-    if (this._previousContext === context) {
-      return this._cachedResult;
+    toggleDropdown($event:MouseEvent):void {
+        $event.preventDefault();
+        $event.stopPropagation();
+        this.status.isopen = !this.status.isopen;
     }
 
-    this._previousContext = context;
-    let f = this.getDrugs('add');
-    // let f:Function = function ():Promise<string[]> {
-    //   let p:Promise<string[]> = new Promise((resolve:Function) => {
-    //     setTimeout(() => {
-    //       let query = new RegExp(context.asyncSelectedCar, 'ig');
-    //       return resolve(context.carsExample1.filter((state:any) => {
-    //         return query.test(state);
-    //       }));
-    //     }, 500);
-    //   });
-    //   return p;
-    // };
-    this._cachedResult = f;
-    return this._cachedResult;
-  }
+    constructor(http:Http) {
+        this.http = http;
+    }
 
-  changeAutocompleteLoading(e:boolean) {
-    this.autocompleteLoading = e;
-  }
+    getDrugs(text) {
+        this.doses = [];
+        return this.callApi('http://doc-and-i-api.herokuapp.com/api/v1/drugs?name_prefix=' + text).map(res => res.json()).map((el)=> {
+            return el.map((data)=> {
+                this.doses.push(data.strength.description);
+                return ({id: data.name, name: data.name}); // unnecessary as format is the same in this case
+            });
+        }).toPromise();
+    }
 
-  changeAutocompleteNoResults(e:boolean) {
-    this.autocompleteNoResults = e;
-  }
+    getUsers() {
+        this.callApi('http://doc-and-i-api.herokuapp.com/api/v1/pharmacies?distance_from_zip=60640');
+    }
 
-  autocompleteOnSelect(e:any) {
-    console.log(`Selected value: ${e.item}`);
-  }
+    callApi(url) {
+        var authHeader = new Headers();
+        authHeader.append('X-Api-Key', '9a04cee7-09d3-4372-8f56-1713ac445fff');
+
+        var options = new RequestOptions({headers: authHeader});
+        return this.http.get(url, options);
+    }
+
+    getCurrentContext() {
+        return this;
+    }
+
+    getAsyncData(context:any):Function {
+        if (this.searchString.length >= 3) {
+            return this.getDrugs(this.searchString);
+        }
+    }
+
+    changeTypeaheadLoading(e:boolean) {
+        this.typeaheadLoading = e;
+    }
+
+    changeTypeaheadNoResults(e:boolean) {
+        this.typeaheadNoResults = e;
+    }
+
+    typeaheadOnSelect(e:any) {
+        console.log(`Selected value: ${e.item.id}`);
+    }
 }
