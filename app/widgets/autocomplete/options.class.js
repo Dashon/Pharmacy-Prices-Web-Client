8c7d8c66460abf3352732374e9ddf64819feac89
Dashon Howard <dhoward@pathf.com>
2016-06-24 01:36:06 -0500

    remove compiled files

--- app/widgets/autocomplete/options.class.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var AutocompleteOptions = (function () {
-    function AutocompleteOptions(options) {
-        Object.assign(this, options);
-    }
-    return AutocompleteOptions;
-}());
-exports.AutocompleteOptions = AutocompleteOptions;
-//# sourceMappingURL=options.class.js.map
